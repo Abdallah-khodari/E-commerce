009bb08d4a14e0ad91733e2902a04d22c8fecb2d
Abdallah khodari <abdallahkhodari35@gmail.com>
2025-02-07 22:10:18 +0200

    Update Paynow.jsx

--- src/Components/Paynow/Paynow.jsx
+++ src/Components/Paynow/Paynow.jsx
@@ -88,7 +88,7 @@ export default function Paynow() {
     }
     axios
       .post(
-        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${CartId}?url=http://localhost:5173`,
+        `https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${CartId}?url=http://https://e-commerce-three-lyart.vercel.app`,
         {
           shippingAddress: values,
         },
